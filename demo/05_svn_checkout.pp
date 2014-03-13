c91ee129e53e506f0b1faf9f5b3431c7df4a5177
Arnout Boks <arnoutboks@gmail.com>
2014-03-13 15:47:07 +0100

    Fixed wrong type of quotes

--- demo/05_svn_checkout.pp
+++ demo/05_svn_checkout.pp
@@ -2,5 +2,5 @@ $repository_url = 'https://github.com/sebastianbergmann/phpcpd/trunk'
 $local_path = '/home/moxio/phpcpd'
 
 exec { 'checkout-phpcpd':
-	command => '/usr/bin/svn checkout ${repository_url} ${local_path}',
+	command => "/usr/bin/svn checkout ${repository_url} ${local_path}",
 }
--- demo/06_svn_checkout_idempotent.pp
+++ demo/06_svn_checkout_idempotent.pp
@@ -2,6 +2,6 @@ $repository_url = 'https://github.com/sebastianbergmann/phpcpd/trunk'
 $local_path = '/home/moxio/phpcpd'
 
 exec { 'checkout-phpcpd':
-	command => '/usr/bin/svn checkout ${repository_url} ${local_path}',
+	command => "/usr/bin/svn checkout ${repository_url} ${local_path}",
 	creates => $local_path
 }
